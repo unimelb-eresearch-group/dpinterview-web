2ed5354b5446b7009a3faa5529b01e4174344cd1
Christopher Bayliss <christopher.bayliss@unimelb.edu.au>
2025-08-29 14:50:21 +1000

    feat: switches out old db config
    Simplifies database client setup as with [env.ts](src/env.ts) the environment variables are baked into the type system
    so the server won't start without at least plausible values set. This lets us just shove them at the PostgreSQL client
    at init time and avoid having to cope with the client maybe not being initialised.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,27 +1,11 @@
-import {URL} from "node:url";
 import {env} from "@/env";
-import {Pool, PoolConfig} from 'pg';
+import {Pool} from 'pg';
 
-const connection: Pool = new Pool(makeConfig());
-
-function makeConfig(): PoolConfig {
-    const dbUrl = new URL(env.DATABASE_URL);
-    return {
-        user: dbUrl.username,
-        host: dbUrl.host,
-        database: dbUrl.pathname,
-        password: dbUrl.password,
-        port: parseInt(dbUrl.port),
-        ssl: {
-            rejectUnauthorized: false,
-        }
-    };
-}
+const connection: Pool = new Pool({
+    connectionString: env.DATABASE_URL,
+});
 
 export function getConnection(): Pool {
-    if (!connection) {
-        throw new Error("Database connection is undefined");
-    }
     return connection;
 }
 
